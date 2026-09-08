8d5b52689c82619a248e7892b67447da6da076f0
Sahibzada Muhammad Hamza <sahibzadaxhamza@gmail.com>
2026-09-09 00:00:56 +0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n>=1 && n<=999){
+            return 0;
+        }
+        int count=0;
+        if(n>999 && n<=100000){
+            return n-999;
+        }
+        
+        return 0;
+    }
+};
